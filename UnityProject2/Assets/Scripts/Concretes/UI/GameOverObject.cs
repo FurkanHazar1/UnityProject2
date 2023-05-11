c12f9338a56398db7f522f40bece81daa0b1b466
FurkanHazar1 <a_furkan_hazar2002@hotmail.com>
2023-05-11 13:49:32 +0300

    GameOver
    Game over uı kısmı ve script kısmı tamamlandı

---
+++ UnityProject2/Assets/Scripts/Concretes/UI/GameOverObject.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityProject2.managers;
+
+
+
+namespace UnityProject2.ui
+{
+
+    public class GameOverObject : MonoBehaviour
+    {
+        [SerializeField] GameObject gameOverPanel;
+        private void Awake()
+        {
+            if (gameOverPanel.activeSelf)
+            {
+                gameOverPanel.SetActive(false);
+            }
+        }
+        private void OnEnable()
+        {
+            GameManager.instance.onGameOver += handleGameOverPanel;
+        }
+        private void OnDisable()
+        {
+            GameManager.instance.onGameOver -= handleGameOverPanel;
+        }
+
+        public void handleGameOverPanel()
+        {
+            if (!gameOverPanel.activeSelf)
+            {
+                gameOverPanel.SetActive(true);
+            }
+        }
+
+
+    }
+
+}
